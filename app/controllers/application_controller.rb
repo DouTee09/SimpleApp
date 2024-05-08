9fd5fd18e08299e3f07faa096e7e1a534ac1ac9a
tungtk6 <tungtk6@example.com>
2024-05-08 11:02:43 +0700

    Update Chapter 3: MOSTLY STATIC PAGES

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,4 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "hello, world!"
   end
-  def home; end
-  def help; end
-  def about; end
-  def contact; end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,6 @@
 class StaticPagesController < ActionController::Base
-  def hello
-    render html: "hello, world!"
-  end
   def home; end
   def help; end
+  def about; end
+  def contact; end
 end
