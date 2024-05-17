dcfcbd0bc7e4fd5bce7588f87c81bb1e6e84a071
tungtk6 <tungtk6@example.com>
2024-05-17 15:36:25 +0700

    Chapter_12: Password reset

--- db/migrate/20240516084509_remove_column_active_and_active_digest_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumnActiveAndActiveDigestFromUsers < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :users, :active, :boolean
-    remove_column :users, :active_digest, :string
-  end
-end
