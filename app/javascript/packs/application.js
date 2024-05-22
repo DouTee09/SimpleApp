73e3666aadfe557a2b008b61e4eb4f27d6d8a122
tungtk6 <tungtk6@example.com>
2024-05-22 11:09:10 +0700

    Chapter 14: FOLLOWING USERS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,3 +12,11 @@ import "bootstrap"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$("#micropost_image").bind("change", function() {
+    const size_in_megabytes = this.files[0].size/1024/1024;
+    if (size_in_megabytes > 5) {
+        alert("Maximum file size is 5MB. Please choose a smaller file.");
+        $("#micropost_image").val("");
+    }
+});
